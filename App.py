66eb08e506bec08a3392c968a64f3845c8717ee9
Surya-git-enf <s8296659@gmail.com>
2025-08-25 19:18:40 +0530

    Update App.py

--- App.py
+++ App.py
@@ -13,10 +13,10 @@ api_hash = os.getenv("API_HASH")
 SUPABASE_URL =    "https://nepbpzskrtjrityzenig.supabase.co"             #OUR SUPABASE PROJECT URL
 SUPABASE_SECRRET_KEY =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lcGJwenNrcnRqcml0eXplbmlnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTUxMDMxOSwiZXhwIjoyMDcxMDg2MzE5fQ.l5lwIsyiiShRT2UPHu4xvc6mRxIhJvNgVM6zpiJhfO0"      #YOUR SUPABASE SECRET ROLE KEY
 supabase: Client = create_client(SUPABASE_URL,SUPABASE_SECRRET_KEY)  # create client for supabase
-#sessions = supabase.table("telegram_sessions").select("Session_string").execute()
+#session_string = supabase.table("telegram_sessions").select("Session_string").execute()
 # Create Telegram client
 
-client = TelegramClient(StringSession(session_string), api_id, api_hash)
+#client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
 # Source and Target Channels from env (comma-separated)
 source_channels = os.getenv("SOURCE_CHANNELS", "").split(",")
@@ -90,4 +90,4 @@ async def main():
 @app.on_event("startup")
 async def startup_event():
     await client.start()
-    asyncio.create_task(forward_messages())
+    asyncio.create_task(main())
