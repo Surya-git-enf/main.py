3152cb43341cbfef9e6cc21a48e14923153e8464
Surya-git-enf <s8296659@gmail.com>
2025-08-27 18:01:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -20,12 +20,6 @@ supabase: Client = create_client(SUPABASE_URL,SUPABASE_KEY)
 
 # Create FastAPI app
 app = FastAPI()
-source = supabase.table("telegram_sessions").select("source_channels").execute() #source channel in telegram_sessions
-target = supabase.table("telegram_sessions").select("target_channels").execute() #target channel in telegram sessions
-sou = source.data[0]["source_channels"] # data in source channels
-tar = target.data[0]["target_channels"] # data in target channels
-source_channels = sou[0].split(",") # to split for array
-target_channels = tar[0].split(",") # to split with , for array
 
 
 @app.get("/")
@@ -35,20 +29,30 @@ def home():
 
 # Background task to forward messages
 async def forward_messages(session_string):
+
+    client = TelegramClient(StringSession(session_string), api_id, api_hash)
+    await client.start()
+
     
+    for i in range(0,len(sou)):
+        source = supabase.table("telegram_sessions").select("source_channels").execute() #source channel in telegram_sessions
+        target = supabase.table("telegram_sessions").select("target_channels").execute() #target channel in telegram sessions
+        sou = source.data[0]["source_channels"] # data in source channels
+        tar = target.data[0]["target_channels"] # data in target channels
+        source_channels = sou[i].split(",") # to split for array
+        target_channels = tar[i].split(",") # to split with , for array
+        
     #source = supabase.table("telegram_sessions").select("source_channels").execute() #source channel in telegram_sessions
     #target = supabase.table("telegram_sessions").select("target_channels").execute() #target channel in telegram sessions
     #sou = source.data[0]["source_channels"] # data in source channels
     #tar = target.data[0]["target_channels"] # data in target channels
-    #source_channels = sou.split(",") # to split for array
-    #target_channels = tar.split(",") # to split with , for array
-
-    client = TelegramClient(StringSession(session_string), api_id, api_hash)
-    await client.start()
- 
-    while True:
-        
-
+    #source_channels = sou[0].split(",") # to split for array
+    #target_channels = tar[0].split(",") # to split with , for array
+                
+                
+    #while True:
+            
+            
         for src in source_channels:
             if not src.strip():
                 continue
