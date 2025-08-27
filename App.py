b9f611d3d69912321630413ef813cb8dba7bc887
Surya-git-enf <s8296659@gmail.com>
2025-08-27 20:35:33 +0530

    Update App.py

--- App.py
+++ App.py
@@ -47,8 +47,8 @@ async def forward_messages(session_string):
         #target = supabase.table("telegram_sessions").select("target_channels").execute() #target channel in telegram sessions
         #sou = source.data[0]["source_channels"] # data in source channels
         #tar = target.data[0]["target_channels"] # data in target channels
-    source_channels = sou[0].split(",") # to split for array
-    target_channels = tar[0].split(",") # to split with , for array
+    source_channels = sou[1].split(",") # to split for array
+    target_channels = tar[1].split(",") # to split with , for array
         
         
         
